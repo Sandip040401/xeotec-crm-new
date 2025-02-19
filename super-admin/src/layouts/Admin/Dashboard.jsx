a32ab524aed8484ed347afa107ba193e9fa17d1e
Shamim Imran <shamimimran35@gmail.com>
2025-02-19 18:50:13 +0530

    feat: restructure project with new admin layout and routing, remove unused files

---
+++ super-admin/src/layouts/Admin/Dashboard.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "@/pages/Admin/Sidebar";
+
+const Dashboard = () => {
+  return (
+    <div className="flex flex-col md:flex-row h-screen">
+      <Sidebar />
+      <main className="flex-1 p-6">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Dashboard;
