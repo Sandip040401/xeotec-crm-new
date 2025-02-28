d176bd4c7a02f82b185122e3359ca15efb274ca7
Anupam Kumar Singh <aksingh2486@gmail.com>
2025-02-28 21:59:41 +0530

    add admin, user login(cookie); RBAC middleware validation

--- backend/modules/CRM/User/controllers/userController.js
+++ backend/modules/CRM/User/controllers/userController.js
@@ -7,27 +7,74 @@ const User = require("../models/User");
 const Company = require("../../Company/models/Company");
 const Permission = require("../../Permission/models/Permission");
 const Role = require("../../Permission/models/Role");
+const generatePassword = require("../../../../common/utils/passwordGenerator");
+const SuperAdmin = require("../../../SuperAdmin/models/SuperAdmin");
+const userGenerationTemplates = require("../../Email/userGenerationTemplates");
+const sendMail = require("../../Email/emailService");
+const Department = require("../../Company/models/Department");
+
+const checkUserExists = async (email, companyId) => {
+  try {
+    const [userA, userB] = await Promise.all([
+      User.findOne({ email, companyId }),
+      SuperAdmin.findOne({ email, companyId }),
+    ]);
+
+    return userA || userB ? true : false;
+  } catch (error) {
+    console.error("Error checking email:", error);
+    return false;
+  }
+};
 
 // company-user
 const createUser = async (req, res) => {
   try {
-    const { email, password, name, userType, companyId } = req.body;
+    const { email, name, departmentId } = req.body;
+    const companyId = req.user.companyId;
+
+    const existingUser = await checkUserExists(email, companyId);
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ success: false, error: "User existing for Company" });
+    }
+
+    const password = generatePassword(8);
 
     //hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-    const user = await User.create({
+    const user = new User({
       email,
       password: hashedPassword,
       name,
-      userType,
       companyId,
     });
 
-    const company = await Company.findById(companyId);
+    user.departments.push(departmentId);
 
+    const company = await Company.findById(companyId);
+    const department = await Department.findById(departmentId);
+
+    department.employees.push(user._id);
     company.employees.push(user._id);
     await company.save();
+    await user.save();
+    await department.save();
+
+    const emailTemplate = userGenerationTemplates.userCreation(
+      user.name,
+      user.email,
+      password
+    );
+
+    sendMail({
+      to: user.email,
+      subject: "User Account Created",
+      templateData: emailTemplate,
+    });
 
     res.status(201).json({
       success: true,
@@ -43,7 +90,15 @@ const createUser = async (req, res) => {
 
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.find({
+      companyId: req.user.companyId,
+    })
+      .populate("departments") // Populating Departments
+      .populate("manager", "name email") // Populating Manager (Selecting only name & email)
+      .populate("roles") // Populating Roles
+      .populate("customPermissions.permissionId") // Populating customPermissions.permissionId
+      .populate("companyId"); // Populating Company
+
     res.json({
       success: true,
       data: users,
@@ -62,6 +117,48 @@ const assignRole = async (req, res) => {
     const user = await User.findById(userId);
 
     user.roles.push(roleId);
+    await user.save();
+
+    res.json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+const assignDepartment = async (req, res) => {
+  try {
+    const { userId, departmentId } = req.body;
+    const user = await User.findById(userId).populate("departments");
+
+    user.departments.push(departmentId);
+    await user.save();
+
+    res.json({
+      success: true,
+      message: "Department assigned successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+const revokeRole = async (req, res) => {
+  try {
+    const { userId, roleId } = req.body;
+    const user = await User.findById(userId);
+
+    user.roles = user.roles.filter((role) => role.toString() !== roleId);
+
     await user.save();
 
     res.json({
@@ -104,27 +201,6 @@ const revokePermission = async (req, res) => {
     user.customPermissions = user.customPermissions.filter(
       (perm) => perm.permissionId.toString() !== permissionId
     );
-    await user.save();
-
-    res.json({
-      success: true,
-      data: user,
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      error: error.message,
-    });
-  }
-};
-
-const revokeRole = async (req, res) => {
-  try {
-    const { userId, roleId } = req.body;
-    const user = await User.findById(userId);
-
-    user.roles = user.roles.filter((role) => role.toString() !== roleId);
-
     await user.save();
 
     res.json({
@@ -201,6 +277,7 @@ const getUserPermissions = async (req, res) => {
 module.exports = {
   createUser,
   getUsers,
+  assignDepartment,
   assignRole,
   assignPermission,
   revokePermission,
--- backend/modules/CRM/User/routes/userRoutes.js
+++ backend/modules/CRM/User/routes/userRoutes.js
@@ -5,11 +5,21 @@ const {
   assignRole,
   getUserPermissions,
 } = require("../controllers/userController.js");
+const verifyToken = require("../../../../common/middlewares/verifyToken.js");
+const rbacMiddleware = require("../../../../common/middlewares/rbacMiddleware.js");
+const validate = require("../../../../common/middlewares/validate.js");
+const userSchema = require("../validations/userValidations.js");
 
 const userRouter = express.Router();
 
-userRouter.post("/user/create", createUser);
-userRouter.get("/user", getUsers);
+userRouter.post(
+  "/user/create",
+  validate(userSchema),
+  verifyToken,
+  rbacMiddleware("hr:user:create"),
+  createUser
+);
+userRouter.get("/user", verifyToken, rbacMiddleware("hr:user:view"), getUsers);
 
 userRouter.post("/user/assign-role", assignRole);
 userRouter.get("/user/permissions", getUserPermissions);
